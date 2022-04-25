d315c752835703a7e2458d9ea097173ffa654430
Alex_Apostol <apostol516@gmail.com>
2022-04-25 21:20:02 +0200

    origin/develop add serviceProxy

---
+++ PruebaTecnicaAccenture/PruebaTecnicaAccenture/Entities/DtoMarvel.swift
@@ -0,0 +1,92 @@
+//
+//  DtoMarvel.swift
+//  PruebaTecnicaAccenture
+//
+//  Created by Alex Apostol on 25/4/22.
+//
+
+import Foundation
+
+// MARK: - DtoMarvel
+struct DtoMarvel: Codable {
+    let code: Int?
+    let status, copyright, attributionText, attributionHTML: String?
+    let etag: String?
+    let data: DataClass?
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let offset, limit, total, count: Int?
+    let results: [CharacterResult]?
+}
+
+// MARK: - Result
+struct CharacterResult: Codable {
+    let id: Int?
+    let name, resultDescription: String?
+    let modified: Date?
+    let thumbnail: Thumbnail?
+    let resourceURI: String?
+    let comics, series: Comics?
+    let stories: Stories?
+    let events: Comics?
+    let urls: [URLElement]?
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case resultDescription = "description"
+        case modified, thumbnail, resourceURI, comics, series, stories, events, urls
+    }
+}
+
+// MARK: - Comics
+struct Comics: Codable {
+    let available: Int?
+    let collectionURI: String?
+    let items: [ComicsItem]?
+    let returned: Int?
+}
+
+// MARK: - ComicsItem
+struct ComicsItem: Codable {
+    let resourceURI: String?
+    let name: String?
+}
+
+// MARK: - Stories
+struct Stories: Codable {
+    let available: Int?
+    let collectionURI: String?
+    let items: [StoriesItem]?
+    let returned: Int?
+}
+
+// MARK: - StoriesItem
+struct StoriesItem: Codable {
+    let resourceURI: String?
+    let name: String?
+    let type: StoriesType?
+}
+
+enum StoriesType: String, Codable {
+    case cover = "cover"
+    case interiorStory = "interiorStory"
+}
+
+// MARK: - Thumbnail
+struct Thumbnail: Codable {
+    let path: String?
+    let thumbnailExtension: String?
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
+
+// MARK: - URLElement
+struct URLElement: Codable {
+    let type: String?
+    let url: String?
+}
